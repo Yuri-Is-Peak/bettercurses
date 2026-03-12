3894c7fe22f6ae24da59bcb641358a28746c6b25
Yuri-Is-Peak <YuriIsPeak@proton.me>
2026-03-12 20:41:07 +0200

    rewriting.

--- README.md
+++ README.md
@@ -1,49 +1,40 @@
 # bettercurses
-bettercurses is an attempt at making an easier to use option for making cli's similar to the ncurses library. 
-
-It provides easier color support, easy to use support for partial screens, and a stricter error handling which helps you detect problems easier and earlier. 
+bettercurses is an attempt at making an easier to use option for making cli's similar to the ncurses library. It provides an improvement over ncurses in the following areas:
+### Easy color support
+### Partial screens
+### Stricter error handling 
 
 # Usage
-The coordinate system is the exact same as most other libraries including curses. The collumns being vertical rows being horizontal. Here's an example:
+The coordinate system is a standard X, Y system. X is the collumns and Y is the rows. The library provides you with globals called maxx and maxy which are updated when you call:
+```c
+getmaxyx(); // updates global maxx and maxy variables
+```
+Note that these variables are not the internal ones used for error validation and other things. You can do anyting you want with them and it shouldn't break anything.
 
-TODO: ADD IMAGE
+The library *currently* has to main systems for initialization of a screen:
+### Fullscreen
 
-The library has to main systems for initialization of a screen
+To initialize a Fullscreen just use the init_fullscreen() function:
+```C
+init_fullscreen(); // Clears the screen and sets up the coordinate grid
+```
 
-Partial
-
-TODO: ADD IMAGE
-
-And Fullscreen
-
-TODO: ADD IMAGE
-
-To initialize a Fullscreen just use the
-
-add later with the code thing
+### Partial_Screen
 
 To initialize a partial screen do:
-
-add later with the code thing
-
+```C
+init_partial_screen(num); // sends num amount of newlines and sets internal variables
+// num must be a positive integer larger then zero.
+```
 The initialization is by how many lines to do below the users prompt box. It is reccomended to set this to a ratio to make it scale with varying sizes by doing:
-
-do the code thing marker
-init_partial_screen(cols / 5)
+```C
+init_partial_screen(round(cols / 5))
 // initializes a partial screen the height of 20% of the users terminals height
+```
+Please avoid initializing more then one screen before the previous one has been discarded. This will lead to unwanted consequences.
 
-
-Note that while you aren't forced to initialize a screen as you can still use functions with a... warying amount of success.
-(You probably should just initialize a screen)
-
-
-
-
-
-
-
-
-
-
-
-
+Note that while you aren't forced to initialize a screen as you can still use functions with a... warying amount of success. Something like:
+```C
+getmaxyx();
+```
+Will still work, however a lot of functions like add_str depend on the screen being initialized, as the error handling is embedded in most functions and it relies on internal variables which wont exist if a screen isnt initialized.
--- bettercurses.c
+++ bettercurses.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <sys/ioctl.h>
+#include <string.h>
 
 #define OCTAL_ESC "\033["
 /* This code has a [
@@ -15,215 +16,42 @@
 #define OCTAL_ESC_ALT "\033"
 #define SAVE_SCR "?1049h"
 #define RESTORE_SCR "?1049l"
-
-
-/* TODO:
- * functions:
- * INTERNAL ONLY
- * add validation of coordinates before printing
- *
- * EXTERNAL
- * add toggle for echoing text
- * add handling of character input (progress)
- * add a border draw function, taking the dimensions and the characters to draw it with
- *
- * DONE
- * add getmaxyx for grabbing terminal dimensions (partially done: havent finished the correct return format)
- * add support for full screen initializing (done)
- * add suport for partial screen initializing (done)
- * add move_cursor (done)
- * add exit_cleanup partially done: might need to add more to function later
- * add_str (done)
- * add_float (done)
- * add_int (done)
- * add_float (done)
- * add_colored_str
- * add cursor_visibility toggle
- */
-// GLOBALS
-int maxy;
-int maxx;
-static int partial_maxy;
-static int internal_maxx;
-static int internal_maxy;
-static bool partial_screen;
-static volatile sig_atomic_t resize_flag = 0;
-
-
-
-void init_fullscreen()
-{
-	partial_screen = false;
-	getmaxyx();
-	printf("%s%s",OCTAL_ESC,SAVE_SCR);
-}
-
-
-static void sigwinch_handler(int sig) {
-    resize_flag = 1;
-}
-
-
-void getmaxyx()
-{
-    struct winsize ws;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
-    maxx = ws.ws_col;
-    maxy = ws.ws_row;
-	internal_maxx = ws.ws_col;
-    internal_maxy = ws.ws_row;
-}
-
-
-
-void cursor_visibility(bool value)
-{
-	if (value){printf("%s?25h",OCTAL_ESC);}
-	else {printf("%s?25l",OCTAL_ESC);}
-}
-
-// rant below
-// Temporarily commented out. Implementing turned out to be a lot harder,
-// Due to some terminals not accepting an older escape sequence to save only the screen
-// We will have to manually track the cursor and delete the newlines made by partial screen. 
-// This is a PAIN IN THE ASS
-// hence why i am not doing it yet and trying to get a functional state first.
-/* void init_partial_screen(int y, bool restore)
-{
-    partial_screen = true;
-    partial_maxy = internal_maxy - y;
-    for (int i = 0; i < y; i++) printf("\n");
-    printf("%s%dA", OCTAL_ESC, y);  // move cursor back up
-    if (restore) printf("%s7", OCTAL_ESC_ALT);  // save position after scroll
-}
-*/
-
-static int internal_validate_coordinates(int x, int y)
-{
-	if (x < internal_maxx && y < internal_maxy) {return 1;}
-	else {return 0;}
-}
-
-
-void move_cursor(int x, int y)
-{
-	x++;
-	y++;
-	printf("%s%d;%dH",OCTAL_ESC,y,x);
-}
-
-
-void internal_move_cursor(int x, int y)
-{
-	printf("%s%d;%dH",OCTAL_ESC,y,x);
-}
-
-
-
+#define VISIBLE_CURSOR "?25h"
+#define INVISIBLE_CURSOR "?25l"
 
 /* 1: block blinking
  * 2: block not blink 
  * 3: underline blink 
  * 4: underline not blink 
  * 5: blinking line 
- * 6: not blinking line
- */
-//this no work for some reason
-void cursor_mode(int mode, bool Blink)
-{
-	if (mode < 0 || mode > 2) {printf("Incorrect mode");}
-	int real_mode = (mode * 2) + (Blink ? 1 : 2);
-	printf("%s%d q",OCTAL_ESC,real_mode);
-    fflush(stdout);
-}
-
-/* EXPLANATION FOR THE "y/x++;"
- * The terminals coordinates are based on:
- * count of lines, not the index of lines. Example:
- * 1 is the first valid line for the count, while for the index its 0.
- * For the design, i ended up picking to have index based coordinates for the library.
- * It can be easily patched not to have index based coordinates by:
- * Removing all lines with "y++;" and "x++;"
- */
-// start of "add" functions
-void add_int(int x, int y, int num)
-{
-	y++;
-	x++;
-	printf("%s7",OCTAL_ESC_ALT);
-	internal_move_cursor(x,y);
-	printf("%d",num);
-	printf("%s8",OCTAL_ESC_ALT);
-}
+ * 6: not blinking line */
+#define CURSOR_MODE " q"
+/* SAVE_CURSOR
+ * OCTAL_ESC_ALT7 to save cursor pos
+ * OCTAL_ESC_ALT8 to load the saved cursor pos */
 
 
-void add_float(int x, int y, float flt)
-{
-	y++;
-	x++;
-	printf("%s7",OCTAL_ESC_ALT);
-	internal_move_cursor(x,y);
-	printf("%f",flt);
-	printf("%s8",OCTAL_ESC_ALT);
-}
 
 
-void add_str(int x, int y, char text[])
-{
-	y++;
-	x++;
-	printf("%s7",OCTAL_ESC_ALT);
-	internal_move_cursor(x,y);
-	printf("%s",text);
-	printf("%s8",OCTAL_ESC_ALT);
-}
 
 
-void add_colored_str(int x, int y, char text[], int red, int green, int blue)
-{
-	y++;
-	x++;
-	printf("%s7",OCTAL_ESC_ALT);
-	internal_move_cursor(x,y);
-	printf("%s38;2;%d;%d;%dm",OCTAL_ESC,red,green,blue);
-	printf("%s",text);
-
-	// Reset styles and colors
-	printf("%s0m",OCTAL_ESC);
-
-	printf("%s8",OCTAL_ESC_ALT);
-}
-// end of "add" functions
 
 
-void exit_cleanup()
-{
-	if (!partial_screen) 
-	{
-		printf("%s%s",OCTAL_ESC,RESTORE_SCR);
-	}
-	else 
-	{
-		// TO FIX
-		printf("%s8",OCTAL_ESC_ALT);
-	}
-	printf("%s0 q",OCTAL_ESC);
-	// echo_input(true)
-	cursor_visibility(true);
-	signal(SIGWINCH, SIG_DFL); 	// kills the signal handling the resizing
-}
 
 
-bool resize_check()
-{
-	if (resize_flag) {resize_flag= 0; return true;}
-	else {return false;}
-}
 
 
-// TODO
-char* get_key() 
-{
-	if (resize_check()) {return "RESIZE";}
-	else {return 0;}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
